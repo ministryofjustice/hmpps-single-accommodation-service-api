5bc8734533f01e9ee0d8e4fc4dc8a8c6c7caa7d3
runner <runner@hmpps-github-actions-runner-757d9dc8b6-cfdzc>
2025-11-04 15:25:32 +0000

    Rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-single-accommodation-service-api"
--- src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/SingleAccommodationServiceApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/SingleAccommodationServiceApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class SingleAccommodationServiceApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<SingleAccommodationServiceApi>(*args)
 }
