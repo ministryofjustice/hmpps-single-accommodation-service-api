08e63c357142a003a97e9da6d98a4f2d9c1a6035
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2025-11-12 17:24:55 +0000

    Add produces json to endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/DummyController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/DummyController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DummyController {
 
-  @GetMapping("/hello-world")
+  @GetMapping("/hello-world", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun helloWorld(): ResponseEntity<String> = ResponseEntity.ok(
     """ 
       {"message": "hello world"}
