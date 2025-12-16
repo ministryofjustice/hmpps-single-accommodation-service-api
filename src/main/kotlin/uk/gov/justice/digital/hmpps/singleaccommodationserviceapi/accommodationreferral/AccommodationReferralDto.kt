9e0e32a4a6187ff14cb93c4495d0be998fb069b9
daveawc <168838504+daveawc@users.noreply.github.com>
2025-12-16 15:58:39 +0000

    Refactor SAS code.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/accommodationreferral/AccommodationReferralDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/accommodationreferral/AccommodationReferralDto.kt
@@ -1,48 +1,18 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.accommodationreferral
 
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas1AssessmentStatus
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas1ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas2ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas2v2ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas3ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.TemporaryAccommodationAssessmentStatus
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.Cas1AssessmentStatus
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.Cas2Status
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.CasService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.TemporaryAccommodationAssessmentStatus
 import java.time.Instant
 import java.util.UUID
 
 data class AccommodationReferralDto(
   val id: UUID,
-  val type: CasType,
+  val type: CasService,
   val status: CasReferralStatus,
   val date: Instant,
-) {
-  constructor(casReferral: Cas1ReferralHistory) : this(
-    casReferral.id,
-    CasType.CAS1,
-    CasReferralStatus.from(casReferral.status),
-    casReferral.createdAt,
-  )
-
-  constructor(casReferral: Cas2ReferralHistory) : this(
-    casReferral.id,
-    CasType.CAS2,
-    CasReferralStatus.fromCas2(casReferral.status),
-    casReferral.createdAt,
-  )
-
-  constructor(casReferral: Cas2v2ReferralHistory) : this(
-    casReferral.id,
-    CasType.CAS2v2,
-    CasReferralStatus.fromCas2(casReferral.status),
-    casReferral.createdAt,
-  )
-
-  constructor(casReferral: Cas3ReferralHistory) : this(
-    casReferral.id,
-    CasType.CAS3,
-    CasReferralStatus.from(casReferral.status),
-    casReferral.createdAt,
-  )
-}
+)
 
 enum class CasReferralStatus {
   ACCEPTED,
@@ -54,42 +24,42 @@ enum class CasReferralStatus {
   companion object {
     fun from(status: Cas1AssessmentStatus): CasReferralStatus = when (status) {
       Cas1AssessmentStatus.COMPLETED -> ACCEPTED
+
       Cas1AssessmentStatus.REALLOCATED -> REJECTED
+
       Cas1AssessmentStatus.AWAITING_RESPONSE,
       Cas1AssessmentStatus.IN_PROGRESS,
       Cas1AssessmentStatus.NOT_STARTED,
       -> PENDING
     }
 
+    fun from(status: Cas2Status): CasReferralStatus = when (status) {
+      Cas2Status.PLACE_OFFERED,
+      Cas2Status.OFFER_ACCEPTED,
+      -> ACCEPTED
+
+      Cas2Status.OFFER_DECLINED_OR_WITHDRAWN,
+      Cas2Status.REFERRAL_CANCELLED,
+      Cas2Status.REFERRAL_WITHDRAWN,
+      -> REJECTED
+
+      Cas2Status.MORE_INFORMATION_REQUESTED,
+      Cas2Status.AWAITING_ARRIVAL,
+      Cas2Status.ON_WAITING_LIST,
+      Cas2Status.AWAITING_DECISION,
+      -> PENDING
+    }
+
     fun from(status: TemporaryAccommodationAssessmentStatus): CasReferralStatus = when (status) {
       TemporaryAccommodationAssessmentStatus.READY_TO_PLACE -> ACCEPTED
+
       TemporaryAccommodationAssessmentStatus.CLOSED,
       TemporaryAccommodationAssessmentStatus.REJECTED,
       -> REJECTED
+
       TemporaryAccommodationAssessmentStatus.UNALLOCATED,
       TemporaryAccommodationAssessmentStatus.IN_REVIEW,
       -> PENDING
     }
-
-    private val cas2Map = mapOf(
-      "More information requested" to PENDING,
-      "Place offered" to ACCEPTED,
-      "Awaiting arrival" to PENDING,
-      "Referral cancelled" to REJECTED,
-      "Referral withdrawn" to REJECTED,
-      "Offer accepted" to ACCEPTED,
-      "On waiting list" to PENDING,
-      "Awaiting decision" to PENDING,
-      "Offer declined or withdrawn" to REJECTED,
-    )
-
-    fun fromCas2(status: String): CasReferralStatus = cas2Map[status] ?: throw IllegalArgumentException("Unknown CAS2 referral status: $status")
   }
-}
-
-enum class CasType {
-  CAS1,
-  CAS2,
-  CAS2v2,
-  CAS3,
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/wiremock/ApprovedPremisesMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/wiremock/ApprovedPremisesMockServer.kt
@@ -3,10 +3,9 @@ package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.integration.w
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import org.springframework.http.HttpStatus
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas1ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas2ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas2v2ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.model.Cas3ReferralHistory
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.CasService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.CasStatus
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.client.approvedpremises.ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.utils.JsonHelper.objectMapper
 
 class ApprovedPremisesMockServer : WireMockServer(9992) {
@@ -25,52 +24,14 @@ class ApprovedPremisesMockServer : WireMockServer(9992) {
     )
   }
 
-  fun stubGetCas1ReferralOKResponse(crn: String, response: List<Cas1ReferralHistory>) {
+  fun stubGetReferralOKResponse(
+    casService: CasService,
+    crn: String,
+    response: List<ReferralHistory<CasStatus>>,
+  ) {
     stubFor(
       WireMock
-        .get(WireMock.urlPathEqualTo("/cas1/external/referrals/$crn"))
-        .willReturn(
-          WireMock
-            .aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(HttpStatus.OK.value())
-            .withBody(objectMapper.writeValueAsString(response)),
-        ),
-    )
-  }
-
-  fun stubGetCas2ReferralOKResponse(crn: String, response: List<Cas2ReferralHistory>) {
-    stubFor(
-      WireMock
-        .get(WireMock.urlPathEqualTo("/cas2/external/referrals/$crn"))
-        .willReturn(
-          WireMock
-            .aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(HttpStatus.OK.value())
-            .withBody(objectMapper.writeValueAsString(response)),
-        ),
-    )
-  }
-
-  fun stubGetCas2v2ReferralOKResponse(crn: String, response: List<Cas2v2ReferralHistory>) {
-    stubFor(
-      WireMock
-        .get(WireMock.urlPathEqualTo("/cas2v2/external/referrals/$crn"))
-        .willReturn(
-          WireMock
-            .aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(HttpStatus.OK.value())
-            .withBody(objectMapper.writeValueAsString(response)),
-        ),
-    )
-  }
-
-  fun stubGetCas3ReferralOKResponse(crn: String, response: List<Cas3ReferralHistory>) {
-    stubFor(
-      WireMock
-        .get(WireMock.urlPathEqualTo("/cas3/external/referrals/$crn"))
+        .get(WireMock.urlPathEqualTo("/${casService.name.lowercase()}/external/referrals/$crn"))
         .willReturn(
           WireMock
             .aResponse()
