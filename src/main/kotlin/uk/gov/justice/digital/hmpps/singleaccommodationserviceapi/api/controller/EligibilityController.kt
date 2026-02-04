e4d52f298ead833c579b8dee94f8c8e1cb324280
daveawc <168838504+daveawc@users.noreply.github.com>
2026-02-04 14:54:27 +0000

    removing leftover mock data

--- src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/api/controller/EligibilityController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/api/controller/EligibilityController.kt
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.EligibilityDto
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.EligibilityService
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.toEligibilityDto
 
 @RestController
 class EligibilityController(
@@ -16,17 +15,5 @@ class EligibilityController(
 
   @PreAuthorize("hasRole('ROLE_PROBATION')")
   @GetMapping("/cases/{crn}/eligibility")
-  fun getEligibility(@PathVariable crn: String): ResponseEntity<EligibilityDto> {
-    val eligibility = eligibilityService.getEligibility(crn)
-    return ResponseEntity.ok(
-      toEligibilityDto(
-        crn = crn,
-        cas1 = eligibility.cas1,
-        cas2Hdc = eligibility.cas2Hdc,
-        cas2PrisonBail = eligibility.cas2PrisonBail,
-        cas2CourtBail = eligibility.cas2CourtBail,
-        cas3 = eligibility.cas3,
-      ),
-    )
-  }
+  fun getEligibility(@PathVariable crn: String): ResponseEntity<EligibilityDto> = ResponseEntity.ok(eligibilityService.getEligibility(crn))
 }
