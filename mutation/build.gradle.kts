69a03b1a2c88719d61d3a8ba97d35473b9afb2c3
bethan.dobb <bethan.dobb@justice.gov.uk>
2026-02-12 10:31:29 +0000

    feature/SAS-240-listen-to-tier-events

--- mutation/build.gradle.kts
+++ mutation/build.gradle.kts
@@ -1,6 +1,17 @@
+val hmppsSqsVersion = "6.0.1"
+val shedlockVersion = "7.5.0"
+
 dependencies {
   implementation(project(":common"))
   implementation(project(":infrastructure"))
+//  implementation("org.springframework.boot:spring-boot-starter-webflux")
+//  implementation("org.springframework.boot:spring-boot-starter-cache")
+//  implementation("org.redisson:redisson:4.1.0")
+//  implementation("org.redisson:redisson-spring-cache:4.1.0")
+//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+//  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:$hmppsSqsVersion")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:$shedlockVersion")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
   runtimeOnly("org.postgresql:postgresql")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   implementation("org.flywaydb:flyway-core")
