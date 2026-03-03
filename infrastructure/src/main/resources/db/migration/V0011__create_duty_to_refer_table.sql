387f8e059be25c6ee841ff19db6d8c7f3204a2a7
David <43159505+david-mcdowall@users.noreply.github.com>
2026-03-03 10:51:19 +0000

    SAS-337: Create DTR POST Endpoint + Infra (#130)
    * feature/SAS-337-POST-create-DTR-submissions-and-infra
    * feature/SAS-337-address-PR-feedback
    * feature/SAS-337-dtr-status-and-event-updates

---
+++ infrastructure/src/main/resources/db/migration/V0011__create_duty_to_refer_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE duty_to_refer(
+    id                          UUID NOT NULL,
+    crn                         TEXT NOT NULL,
+    local_authority_area_id     UUID NOT NULL,
+    reference_number            TEXT,
+    submission_date             DATE NOT NULL,
+    status                      VARCHAR(80) NOT NULL,
+    created_by_user_id          UUID,
+    created_at                  TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    last_updated_by_user_id     UUID,
+    last_updated_at             TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY(id)
+);
