4aff3ffffb2f8a640f0737d0c0e68286ee5081ed
daveawc <168838504+daveawc@users.noreply.github.com>
2026-04-23 13:38:17 +0100

    Adding mappings for CAS enum values. (#204)

--- infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/client/approvedpremises/Cas3Application.kt
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/client/approvedpremises/Cas3Application.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import java.util.UUID
 
 data class Cas3Application(
@@ -9,27 +10,48 @@ data class Cas3Application(
   val bookingStatus: Cas3BookingStatus?,
 )
 
-enum class Cas3ApplicationStatus {
-  IN_PROGRESS,
-  SUBMITTED,
-  REQUESTED_FURTHER_INFORMATION,
-  REJECTED,
+enum class Cas3ApplicationStatus(val casValue: String) {
+  IN_PROGRESS("inProgress"),
+  SUBMITTED("submitted"),
+  REQUESTED_FURTHER_INFORMATION("requestedFurtherInformation"),
+  REJECTED("rejected"),
+  ;
+
+  companion object {
+    @JsonCreator
+    @JvmStatic
+    fun from(value: String): Cas3ApplicationStatus = Cas3ApplicationStatus.entries.first { it.casValue == value || it.name == value }
+  }
 }
 
-enum class Cas3AssessmentStatus {
-  UNALLOCATED,
-  IN_REVIEW,
-  READY_TO_PLACE,
-  CLOSED,
-  REJECTED,
+enum class Cas3AssessmentStatus(val casValue: String) {
+  UNALLOCATED("unallocated"),
+  IN_REVIEW("in_review"),
+  READY_TO_PLACE("ready_to_place"),
+  CLOSED("closed"),
+  REJECTED("rejected"),
+  ;
+
+  companion object {
+    @JsonCreator
+    @JvmStatic
+    fun from(value: String): Cas3AssessmentStatus = Cas3AssessmentStatus.entries.first { it.casValue == value || it.name == value }
+  }
 }
 
-enum class Cas3BookingStatus {
-  PROVISIONAL,
-  CONFIRMED,
-  ARRIVED,
-  NOT_MINUS_ARRIVED,
-  DEPARTED,
-  CANCELLED,
-  CLOSED,
+enum class Cas3BookingStatus(val casValue: String) {
+  PROVISIONAL("provisional"),
+  CONFIRMED("rejected"),
+  ARRIVED("arrived"),
+  NOT_MINUS_ARRIVED("notMinusArrived"),
+  DEPARTED("departed"),
+  CANCELLED("cancelled"),
+  CLOSED("closed"),
+  ;
+
+  companion object {
+    @JsonCreator
+    @JvmStatic
+    fun from(value: String): Cas3BookingStatus = Cas3BookingStatus.entries.first { it.casValue == value || it.name == value }
+  }
 }
