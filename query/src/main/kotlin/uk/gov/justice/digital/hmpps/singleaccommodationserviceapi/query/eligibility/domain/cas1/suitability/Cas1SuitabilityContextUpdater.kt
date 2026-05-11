842a3b5457ab75c48d3f5b8bfdc600b17829ed93
bethan.dobb <bethan.dobb@justice.gov.uk>
2026-05-11 11:00:41 +0100

    chore/SAS-541-refactor-context-updaters-to-remove-NOT_ELIGIBLE

--- query/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/eligibility/domain/cas1/suitability/Cas1SuitabilityContextUpdater.kt
+++ query/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/eligibility/domain/cas1/suitability/Cas1SuitabilityContextUpdater.kt
@@ -13,37 +13,25 @@ class Cas1SuitabilityContextUpdater : ContextUpdater() {
 
   override fun toServiceResult(context: EvaluationContext): ServiceResult {
     val applicationStatus = context.data.cas1Application?.applicationStatus
-    val requestForPlacementStatus = context.data.cas1Application?.requestForPlacementStatus
-    val placementStatus = context.data.cas1Application?.placementStatus
 
-    return if (requestForPlacementStatus == null && placementStatus == null) {
-      when (applicationStatus) {
-        Cas1ApplicationStatus.STARTED -> ServiceResult(
-          serviceStatus = ServiceStatus.NOT_SUBMITTED,
-          action = EligibilityKeys.CONTINUE_APPROVED_PREMISE_APPLICATION,
-          link = EligibilityKeys.CONTINUE_APPLICATION,
-        )
+    return when (applicationStatus) {
+      Cas1ApplicationStatus.STARTED -> ServiceResult(
+        serviceStatus = ServiceStatus.NOT_SUBMITTED,
+        action = EligibilityKeys.CONTINUE_APPROVED_PREMISE_APPLICATION,
+        link = EligibilityKeys.CONTINUE_APPLICATION,
+      )
 
-        Cas1ApplicationStatus.REJECTED -> ServiceResult(
-          serviceStatus = ServiceStatus.APPLICATION_REJECTED,
-          action = EligibilityKeys.START_APPROVED_PREMISE_APPLICATION,
-          link = EligibilityKeys.START_NEW_APPLICATION,
-        )
+      Cas1ApplicationStatus.REJECTED -> ServiceResult(
+        serviceStatus = ServiceStatus.APPLICATION_REJECTED,
+        action = EligibilityKeys.START_APPROVED_PREMISE_APPLICATION,
+        link = EligibilityKeys.START_NEW_APPLICATION,
+      )
 
-        Cas1ApplicationStatus.WITHDRAWN,
-        Cas1ApplicationStatus.INAPPLICABLE,
-        Cas1ApplicationStatus.EXPIRED,
-        null,
-        -> ServiceResult(
-          serviceStatus = ServiceStatus.NOT_STARTED,
-          action = EligibilityKeys.START_APPROVED_PREMISE_APPLICATION,
-          link = EligibilityKeys.START_APPLICATION,
-        )
-
-        else -> ServiceResult(serviceStatus = ServiceStatus.NOT_ELIGIBLE)
-      }
-    } else {
-      ServiceResult(serviceStatus = ServiceStatus.NOT_ELIGIBLE)
+      else -> ServiceResult(
+        serviceStatus = ServiceStatus.NOT_STARTED,
+        action = EligibilityKeys.START_APPROVED_PREMISE_APPLICATION,
+        link = EligibilityKeys.START_APPLICATION,
+      )
     }
   }
 }
