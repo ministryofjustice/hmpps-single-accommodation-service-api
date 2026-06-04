0803e57e7f6c5fcd15e2f2e15bfbf19b9f7dff1e
maccalsa <Stuart.MacCallum@justice.go.uk>
2026-06-04 14:24:34 +0100

    SAS-625: Add Sentry interceptor to core

---
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/sentry/SentryRequestContextInterceptor.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.sentry
+
+import io.sentry.IScope
+import io.sentry.ScopeType
+import io.sentry.Sentry
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.HandlerMapping
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.UUID
+
+@Component
+class SentryRequestContextInterceptor : HandlerInterceptor {
+  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    val requestId = UUID.randomUUID().toString()
+    val endpointPattern = request.getPathPattern() ?: request.requestURI
+    val serviceName = request.getHeader("X-Service-Name") ?: "Not specified"
+
+    Sentry.configureScope(ScopeType.ISOLATION) { scope: IScope ->
+      scope.setTag("request.id", requestId)
+      scope.setTag("request.method", request.method)
+      scope.setTag("request.pathPattern", endpointPattern)
+      scope.setTag("request.serviceName", serviceName)
+    }
+
+    return true
+  }
+
+  private fun HttpServletRequest.getPathPattern() = getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE) as? String
+}
+
+@Component
+class SentryRequestContextInterceptorConfig(
+  private val interceptor: SentryRequestContextInterceptor,
+) : WebMvcConfigurer {
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    registry.addInterceptor(interceptor)
+  }
+}
---
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/sentry/TracesSamplerCallback.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.sentry
+
+import io.sentry.SamplingContext
+import io.sentry.SentryOptions
+import org.springframework.http.server.PathContainer
+import org.springframework.stereotype.Component
+import org.springframework.web.util.pattern.PathPatternParser
+
+@Component
+class TracesSamplerCallback : SentryOptions.TracesSamplerCallback {
+  private val ignoredPathPatterns = listOf("/health/**").let { patterns ->
+    val parser = PathPatternParser()
+    patterns.map { pattern -> parser.parse(pattern) }
+  }
+
+  @Suppress("MagicNumber")
+  override fun sample(context: SamplingContext): Double? {
+    val parentSampled = context.transactionContext.parentSampled
+    if (parentSampled != null) {
+      return if (parentSampled) 0.01 else 0.0
+    }
+
+    val path = PathContainer.parsePath(removeHttpMethodPrefix(context.transactionContext.name))
+    if (ignoredPathPatterns.any { it.matches(path) }) {
+      return 0.0
+    }
+
+    return null
+  }
+
+  private fun removeHttpMethodPrefix(transactionName: String) = transactionName
+    .replace("GET ", "")
+    .replace("PUT ", "")
+    .replace("POST ", "")
+    .replace("PATCH ", "")
+    .replace("DELETE ", "")
+}
