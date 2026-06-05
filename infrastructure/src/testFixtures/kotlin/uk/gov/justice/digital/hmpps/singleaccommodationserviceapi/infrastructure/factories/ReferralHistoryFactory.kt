4c34b5a7824ffa93238bf9f154be4ed4346c55d1
daveawc <168838504+daveawc@users.noreply.github.com>
2026-06-05 13:27:22 +0100

    moved factory to same package as model, removed testfixtrues dependency

--- infrastructure/src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/factories/ReferralHistoryFactory.kt
+++ infrastructure/src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/factories/ReferralHistoryFactory.kt
@@ -1,17 +1,10 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.factories
 
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.DtrStatus
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.DtrSubmissionDto
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.DutyToReferDto
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.LocalAuthorityDto
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.OutcomeReason
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.WithdrawalReason
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas1ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas2ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas3ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.DeliusUserDto
 import java.time.Instant
-import java.time.LocalDate
 import java.util.UUID
 
 fun buildReferralHistory(
@@ -96,33 +89,4 @@ fun buildDeliusUserDto(name: String = "Joe Bloggs", username: String = "user1", 
   name = name,
   username = username,
   staffCode = staffCode,
-)
-
-fun buildDutyToReferDto(
-  crn: String,
-  id: UUID = UUID.randomUUID(),
-  caseId: UUID = UUID.randomUUID(),
-  localAuthorityAreaId: UUID = UUID.randomUUID(),
-  referenceNumber: String? = "DTR-REF-001",
-  submissionDate: LocalDate = LocalDate.of(2026, 1, 15),
-  status: DtrStatus = DtrStatus.SUBMITTED,
-  withdrawalReason: WithdrawalReason? = null,
-  withdrawalReasonOther: String? = null,
-  outcomeReason: OutcomeReason? = null,
-  createdAt: Instant = Instant.now(),
-) = DutyToReferDto(
-  caseId,
-  crn,
-  status,
-  submission = DtrSubmissionDto(
-    id = id,
-    localAuthority = LocalAuthorityDto(localAuthorityAreaId, "Test Local Authority"),
-    referenceNumber = referenceNumber,
-    submissionDate = submissionDate,
-    createdBy = "Test User",
-    createdAt = createdAt,
-    withdrawalReason = withdrawalReason,
-    withdrawalReasonOther = withdrawalReasonOther,
-    outcomeReason = outcomeReason,
-  ),
 )
