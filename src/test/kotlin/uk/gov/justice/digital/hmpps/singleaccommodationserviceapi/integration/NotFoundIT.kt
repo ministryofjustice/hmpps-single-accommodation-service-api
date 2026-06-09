58c1b552a830bf389c6b09a41eebacd39c03db3d
daveawc <168838504+daveawc@users.noreply.github.com>
2026-06-09 16:09:29 +0100

    adding another wait onto flaky integration test (#333)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/NotFoundIT.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/NotFoundIT.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.integration
 
+import org.awaitility.kotlin.await
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.integration.wiremock.HmppsAuthStubs
+import java.time.Duration
 
 class NotFoundIT : IntegrationTestBase() {
 
@@ -14,9 +16,16 @@ class NotFoundIT : IntegrationTestBase() {
 
   @Test
   fun `Resources that aren't found should return 404 - test of the exception handler`() {
-    restTestClient.get().uri("/some-url-not-found")
-      .withDeliusUserJwt()
-      .exchange()
-      .expectStatus().isNotFound
+    await
+      .atMost(Duration.ofSeconds(10))
+      .pollInterval(Duration.ofMillis(100))
+      .logging()
+      .untilAsserted {
+        restTestClient.get().uri("/some-url-not-found")
+          .exchange()
+          .expectStatus().isEqualTo(404)
+          .expectBody().jsonPath("$.userMessage")
+          .isEqualTo("No resource found failure: No static resource some-url-not-found for request '/some-url-not-found'.")
+      }
   }
 }
