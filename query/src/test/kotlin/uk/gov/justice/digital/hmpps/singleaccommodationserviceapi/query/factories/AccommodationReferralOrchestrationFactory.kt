8b414b5ab4698cf989a89c20b40f3165da4dafcb
Rashad Mughal <rashadmughal@users.noreply.github.com>
2026-06-15 14:18:36 +0100

    SAS-467: Remove cas2 from referral history

--- query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/factories/AccommodationReferralOrchestrationFactory.kt
+++ query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/factories/AccommodationReferralOrchestrationFactory.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.factories
 
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas1ReferralHistory
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas2ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.approvedpremises.Cas3ReferralHistory
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.factories.buildDeliusUserDto
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.factories.buildReferralHistory
@@ -11,28 +10,6 @@ fun buildAccommodationReferralOrchestrationDto(
   cas1Referrals: List<Cas1ReferralHistory> = listOf(
     buildReferralHistory(
       Cas1ReferralHistory.Cas1AssessmentStatus.COMPLETED,
-      referralRejectionReason = "Some reason",
-      localAuthorityArea = "Some area",
-      pdu = "Some pdu",
-      referredBy = buildDeliusUserDto(),
-      placementAddress = "Some address",
-      placementStatus = "Some status",
-    ),
-  ),
-  cas2HdcReferrals: List<Cas2ReferralHistory> = listOf(
-    buildReferralHistory(
-      Cas2ReferralHistory.Cas2Status.PLACE_OFFERED,
-      referralRejectionReason = "Some reason",
-      localAuthorityArea = "Some area",
-      pdu = "Some pdu",
-      referredBy = buildDeliusUserDto(),
-      placementAddress = "Some address",
-      placementStatus = "Some status",
-    ),
-  ),
-  cas2Referrals: List<Cas2ReferralHistory> = listOf(
-    buildReferralHistory(
-      Cas2ReferralHistory.Cas2Status.AWAITING_DECISION,
       referralRejectionReason = "Some reason",
       localAuthorityArea = "Some area",
       pdu = "Some pdu",
@@ -53,7 +30,5 @@ fun buildAccommodationReferralOrchestrationDto(
   ),
 ) = AccommodationReferralOrchestrationDto(
   cas1Referrals = cas1Referrals,
-  cas2HdcReferrals = cas2HdcReferrals,
-  cas2Referrals = cas2Referrals,
   cas3Referrals = cas3Referrals,
 )
