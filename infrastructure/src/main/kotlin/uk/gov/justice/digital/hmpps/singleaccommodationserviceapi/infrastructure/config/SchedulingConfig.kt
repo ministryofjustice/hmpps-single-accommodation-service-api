b69cdf4ac79add34fea9064afa7ab9f56d3b95af
David Atkins <david.atkins1@justice.gov.uk>
2026-06-18 15:57:45 +0100

    Enable shedlock (#371)
    This commit adds configuration required to enable shed lock. Whilst shedlock was already configured, it was not enabled and as such not being applied

--- infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/config/SchedulingConfig.kt
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/config/SchedulingConfig.kt
@@ -1,10 +1,19 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.config
 
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import javax.sql.DataSource
 
 @ConditionalOnProperty(value = ["scheduling.enabled"], havingValue = "true")
 @Configuration
 @EnableScheduling
-class SchedulingConfig
+@EnableSchedulerLock(defaultLockAtMostFor = $$"${shedlock.default-lock-at-most-for}")
+class SchedulingConfig {
+  @Bean
+  fun lockProvider(dataSource: DataSource): LockProvider = JdbcTemplateLockProvider(dataSource)
+}
