cf59e6ffd878766e5670304e98c591ea4b371d69
Rashad Mughal <rashadmughal@users.noreply.github.com>
2026-07-08 14:37:42 +0100

    Copilot feedback

--- src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/referralhistory/response/GetAccommodationReferralResponses.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/referralhistory/response/GetAccommodationReferralResponses.kt
@@ -20,7 +20,7 @@ fun expectedGetReferralHistory(
      {
         "id":"$dtrId",
         "type":"DTR",
-        "status":"${dtrStatus ?: "DEPARTED"}",
+        "status":"${dtrStatus ?: "NOT_ACCEPTED"}",
         "assessmentStatus": null,
         "requestForPlacementStatus": null,
         "date":"${dtrSubmissionDate}T00:00:00Z",
