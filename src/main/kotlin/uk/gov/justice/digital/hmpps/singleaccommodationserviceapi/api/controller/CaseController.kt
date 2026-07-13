5c4717c4daec55d7f3f8319933e750d2b79cb824
daveawc <168838504+daveawc@users.noreply.github.com>
2026-07-13 13:19:48 +0100

    Add try/catch to create cases
    This batches the inserts and adds a recursive call to save cases outside of the transaction.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/api/controller/CaseController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/api/controller/CaseController.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.api.controller
 
+import org.slf4j.LoggerFactory
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,12 +14,15 @@ import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.common.dtos.Ri
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CaseApplicationService
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CrnToPrisonNumber
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.case.CaseQueryService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.case.PersonDto
 
 @RestController
 class CaseController(
   private val caseQueryService: CaseQueryService,
   private val caseApplicationService: CaseApplicationService,
 ) {
+
+  private val log = LoggerFactory.getLogger(javaClass)
 
   @PreAuthorize("hasAnyRole('SINGLE_ACCOMMODATION_SERVICE_PROBATION_PRACTITIONER')")
   @GetMapping("/case-list")
@@ -30,17 +35,26 @@ class CaseController(
     val upstreamFailures = personDtos.upstreamFailures.toMutableList()
 
     val filteredCaseList = caseQueryService.applyCaseListFilters(personDtos.data, searchTerm, riskLevel, teamCode)
-
-    val unpersistedCrns = caseQueryService.findUnpersistedCrns(filteredCaseList.map { it.crn })
-    if (unpersistedCrns.isNotEmpty()) {
-      val crnsToPrisonNumbers =
-        filteredCaseList.filter { it.crn in unpersistedCrns }.map { CrnToPrisonNumber(it.crn, it.nomsNumber) }
-      caseApplicationService.createCases(crnsToPrisonNumbers)
-    }
-
+    saveCases(filteredCaseList)
     // TODO: Add caseApplicationService.upsertCases() in here after MVP
     val caseDtos = caseQueryService.getCases(filteredCaseList)
     return ResponseEntity.ok(ApiResponseDto(data = caseDtos, upstreamFailures = upstreamFailures))
+  }
+
+  fun saveCases(filteredCaseList: List<PersonDto>) {
+    filteredCaseList.chunked(25) { chunk ->
+      val unpersistedCrns = caseQueryService.findUnpersistedCrns(filteredCaseList.map { it.crn })
+      if (unpersistedCrns.isNotEmpty()) {
+        val crnsToPrisonNumbers =
+          chunk.filter { it.crn in unpersistedCrns }.map { CrnToPrisonNumber(it.crn, it.nomsNumber) }
+        try {
+          caseApplicationService.createCases(crnsToPrisonNumbers)
+        } catch (d: DataIntegrityViolationException) {
+          log.warn("Caught DataIngratityViolation When creating cases. Refiltering:\n{}", d.message)
+          saveCases(chunk)
+        }
+      }
+    }
   }
 
   @PreAuthorize("hasAnyRole('SINGLE_ACCOMMODATION_SERVICE_PROBATION_PRACTITIONER')")
