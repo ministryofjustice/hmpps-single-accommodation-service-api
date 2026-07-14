67d7c026fada45a25a18706e0a53a0cb2ecec753
daveawc <168838504+daveawc@users.noreply.github.com>
2026-07-14 16:38:10 +0100

    Refactor bulk case insert
    This PR refactors the case insert logic and adds retries for any DataExceptionViolations. It also adds unit test coverage in for this.
    The splitting of the case list remains in the controller, so each chunk saves independently.

--- mutation/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/application/service/CaseApplicationService.kt
+++ mutation/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/application/service/CaseApplicationService.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service
 
 import org.slf4j.LoggerFactory
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.tier.Tier
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.entity.CaseEntity
@@ -16,15 +18,34 @@ class CaseApplicationService(
   private val caseOrchestrationService: CaseMutationOrchestrationService,
 ) {
   private val log = LoggerFactory.getLogger(CaseApplicationService::class.java)
+  private val maxAttempts = 3
 
-  @Transactional
-  fun createCases(crns: List<CrnToPrisonNumber>) = crns.map {
-    CaseMapper.create(
-      CaseAggregate.hydrateNew().snapshot(),
-      buildIdentifiers(crn = it.crn, prisonNumber = it.prisonNumber),
-    )
-  }.also { caseRepository.saveAll(it) }
-    .also { log.debug("Creating {} new Cases for: {}", crns.size, crns.map { it.crn }) }
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  fun createCases(crnsToPrisonNumbers: List<CrnToPrisonNumber>) {
+    var count = 0
+    while (count < maxAttempts) {
+      val crnsToPersist = caseRepository.findUnpersistedCrns(crnsToPrisonNumbers.map { it.crn }.toTypedArray())
+      if (crnsToPersist.isEmpty()) return
+      count++
+      val entities = crnsToPrisonNumbers.filter { it.crn in crnsToPersist }.map {
+        CaseMapper.create(
+          CaseAggregate.hydrateNew().snapshot(),
+          buildIdentifiers(crn = it.crn, prisonNumber = it.prisonNumber),
+        )
+      }
+      try {
+        caseRepository.saveAll(entities)
+        break
+      } catch (e: DataIntegrityViolationException) {
+        log.warn("Caught DataIntegrityViolationException. Retrying. {}", e.message)
+        if (count == maxAttempts) {
+          log.warn("Data violation after 3 attempts.")
+          throw e
+        }
+      }
+    }
+    log.debug("Creating {} new Cases for: {}", crnsToPrisonNumbers.size, crnsToPrisonNumbers.map { it.crn })
+  }
 
   @Transactional
   fun upsertCase(crn: String, prisonNumber: String?) = caseRepository.findByIdentifiers(crns = listOf(crn), prisonNumbers = prisonNumber?.let { listOf(it) })
---
+++ mutation/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/unit/service/CaseApplicationServiceTest.kt
@@ -0,0 +1,111 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.unit.service
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.dao.DataIntegrityViolationException
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.entity.CaseEntity
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.repository.CaseRepository
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CaseApplicationService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CaseMutationOrchestrationService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CrnToPrisonNumber
+import java.util.UUID
+
+@ExtendWith(MockKExtension::class)
+class CaseApplicationServiceTest {
+  @MockK
+  lateinit var caseRepository: CaseRepository
+
+  @RelaxedMockK
+  lateinit var caseOrchestrationService: CaseMutationOrchestrationService
+
+  @InjectMockKs
+  lateinit var caseApplicationService: CaseApplicationService
+
+  @Test
+  fun `createCases persists only unpersisted Crns`() {
+    val first = CrnToPrisonNumber(crn = UUID.randomUUID().toString(), prisonNumber = UUID.randomUUID().toString())
+    val second = CrnToPrisonNumber(crn = UUID.randomUUID().toString(), prisonNumber = UUID.randomUUID().toString())
+    val third = CrnToPrisonNumber(crn = UUID.randomUUID().toString(), prisonNumber = UUID.randomUUID().toString())
+    val crnToPrisonNumbers = listOf(first, second, third)
+    val entityList = mutableListOf<List<CaseEntity>>()
+
+    every { caseRepository.findUnpersistedCrns(any()) } returns listOf(first.crn, third.crn)
+    every { caseRepository.saveAll(capture(entityList)) } answers { firstArg() }
+
+    caseApplicationService.createCases(crnToPrisonNumbers)
+
+    verify(exactly = 1) { caseRepository.saveAll(any<List<CaseEntity>>()) }
+    val entities = entityList[0]
+    assertThat(entities).hasSize(2)
+    assertThat(entities.map { it.latestCrn() }).containsExactlyInAnyOrder(first.crn, third.crn)
+    assertThat(entities[0].latestCrn()).isEqualTo(first.crn)
+    assertThat(entities[1].latestCrn()).isEqualTo(third.crn)
+  }
+
+  @Test
+  fun `Does not save when no cases to persist`() {
+    val crnToPrisonNumbers = List(3) {
+      CrnToPrisonNumber(
+        crn = UUID.randomUUID().toString(),
+        prisonNumber = UUID.randomUUID().toString(),
+      )
+    }
+
+    every { caseRepository.findUnpersistedCrns(any()) } returns emptyList()
+
+    caseApplicationService.createCases(crnToPrisonNumbers)
+
+    verify(exactly = 1) { caseRepository.findUnpersistedCrns(any()) }
+    verify(exactly = 0) { caseRepository.saveAll(any<Iterable<CaseEntity>>()) }
+  }
+
+  @Test
+  fun `createCases() retries multiple times on DataIntegrityViolation exception`() {
+    val crnToPrisonNumbers = List(3) {
+      CrnToPrisonNumber(crn = UUID.randomUUID().toString(), prisonNumber = UUID.randomUUID().toString())
+    }
+
+    val result = crnToPrisonNumbers.map { it.crn }
+
+    every { caseRepository.findUnpersistedCrns(any()) } answers { result }
+    every { caseRepository.saveAll(any<Iterable<CaseEntity>>()) } throws
+      DataIntegrityViolationException("duplicate-1") andThenThrows
+      DataIntegrityViolationException("duplicate-2") andThenAnswer {
+        firstArg<Iterable<CaseEntity>>().toMutableList()
+      }
+
+    caseApplicationService.createCases(crnToPrisonNumbers)
+
+    verify(exactly = 3) { caseRepository.saveAll(any<Iterable<CaseEntity>>()) }
+  }
+
+  @Test
+  fun `createCases() throws after 3 retries`() {
+    val crnToPrisonNumbers = List(3) {
+      CrnToPrisonNumber(crn = UUID.randomUUID().toString(), prisonNumber = UUID.randomUUID().toString())
+    }
+
+    val result = crnToPrisonNumbers.map { it.crn }
+
+    every { caseRepository.findUnpersistedCrns(any()) } answers { result }
+    every { caseRepository.saveAll(any<Iterable<CaseEntity>>()) } throws
+      DataIntegrityViolationException("duplicate-1") andThenThrows
+      DataIntegrityViolationException("duplicate-2") andThenThrows
+      DataIntegrityViolationException("duplicate-3")
+
+    assertThrows<DataIntegrityViolationException> {
+      caseApplicationService.createCases(crnToPrisonNumbers)
+    }
+
+    verify(exactly = 3) { caseRepository.findUnpersistedCrns(any()) }
+    verify(exactly = 3) { caseRepository.saveAll(any<Iterable<CaseEntity>>()) }
+  }
+}
