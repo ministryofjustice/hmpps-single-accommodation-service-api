efce27d7869cb3939b39235330e30900e7aaad2c
daveawc <168838504+daveawc@users.noreply.github.com>
2026-07-24 13:17:10 +0100

    added CacheHelper.kt for utility functions and assertions

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/utils/CacheHelper.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.utils
+
+import org.assertj.core.api.Assertions.assertThat
+import org.springframework.cache.CacheManager
+import org.springframework.stereotype.Service
+
+@Service
+class CacheHelper(private val cacheManager: CacheManager) {
+  fun cacheValueByCrn(
+    crn: String,
+    cacheKey: String,
+    cacheValue: Any,
+  ) {
+    cacheManager.getCache(cacheKey)!!.put(crn, cacheValue)
+    assertCacheEntryExists(crn, cacheKey)
+  }
+
+  fun assertCacheEntryExists(crn: String, cacheKey: String) {
+    assertThat(cacheManager.getCache(cacheKey)!!.get(crn)).isNotNull
+  }
+
+  fun assertCacheEntryEvicted(crn: String, cacheKey: String) {
+    assertThat(cacheManager.getCache(cacheKey)!!.get(crn)).isNull()
+  }
+
+  fun isCacheEvicted(
+    crn: String,
+    cacheKey: String,
+  ): Boolean = cacheManager.getCache(cacheKey)!!.get(crn) == null
+}
