53c28c9fd7147fa42392a5cf2d060cdbd82c0850
Fred Marecesche <froddd@users.noreply.github.com>
2026-07-31 10:57:06 +0100

    SAS-850: Order Case list response by surname then forename by default (#506)
    * Order Case list response by surname then forename by default
    
    * Ensure team filter compares any order
    
    * Change test fixture to show ordering is case insensitive

--- query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/unit/case/CaseQueryServiceTest.kt
+++ query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/unit/case/CaseQueryServiceTest.kt
@@ -193,6 +193,35 @@ class CaseQueryServiceTest {
       assertThat(result).noneMatch { it.assignedTo.username == assignedToOther.username }
     }
 
+    @Test
+    fun `orders cases alphabetically by surname then forename by default`() {
+      val unorderedPersonDtos = listOf(
+        buildFullPersonDto(
+          crn = "CRN1",
+          name = buildName(forename = "CHARLIE", surname = "BROWN"),
+          assignedTo = assignedTo,
+        ),
+        buildFullPersonDto(
+          crn = "CRN2",
+          name = buildName(forename = "Zoe", surname = "Adams"),
+          assignedTo = assignedTo,
+        ),
+        buildLimitedPersonDto(
+          crn = "CRN3",
+          assignedTo = assignedTo,
+        ),
+        buildFullPersonDto(
+          crn = "CRN4",
+          name = buildName(forename = "alice", surname = "brown"),
+          assignedTo = assignedTo,
+        ),
+      )
+
+      val result = caseQueryService.applyCaseListFilters(personDtos = unorderedPersonDtos)
+
+      assertThat(result.map { it.crn }).containsExactly("CRN2", "CRN4", "CRN1", "CRN3")
+    }
+
     @ParameterizedTest
     @CsvSource(
       value = [
@@ -298,7 +327,7 @@ class CaseQueryServiceTest {
       assertThat(result).hasSize(count)
       // check we can see cases in other teams
       if (teamCode == "TestTeam2") {
-        assertThat(result.mapNotNull { it.assignedTo.username }.distinct()).containsExactly(username, "Second.User")
+        assertThat(result.mapNotNull { it.assignedTo.username }.distinct()).containsExactlyInAnyOrder("Second.User", username)
       } else {
         assertThat(result).noneMatch { it.assignedTo.username == assignedToOther.username }
       }
