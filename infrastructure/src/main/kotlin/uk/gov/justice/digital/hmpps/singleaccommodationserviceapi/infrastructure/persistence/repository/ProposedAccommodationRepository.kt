70efb56060835a7f8bfae78458f0cabd0927a00a
simonwilson4moj <simon.wilson4@justice.gov.uk>
2026-08-03 16:02:21 +0100

    linting

--- infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/persistence/repository/ProposedAccommodationRepository.kt
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/persistence/repository/ProposedAccommodationRepository.kt
@@ -81,10 +81,12 @@ interface ProposedAccommodationRepository : JpaRepository<ProposedAccommodationE
   )
   fun findAllForSar(caseId: UUID, startDate: Instant, endDate: Instant): List<ProposedAccommodationEntity>
 
-  @Query("""
+  @Query(
+    """
   select pa from ProposedAccommodationEntity pa
   where pa.cprAddressId in :addressIds
   and pa.deleted = false
-""")
+""",
+  )
   fun findByCprAddressIds(addressIds: List<UUID>): List<ProposedAccommodationEntity>
 }
