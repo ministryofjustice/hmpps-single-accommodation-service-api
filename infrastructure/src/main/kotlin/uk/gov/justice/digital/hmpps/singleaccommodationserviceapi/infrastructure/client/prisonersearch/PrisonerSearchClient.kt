28956ae3088b603c16d7c1afc1a546224c5735ee
daveawc <168838504+daveawc@users.noreply.github.com>
2026-08-20 14:34:28 +0100

    fixed unit testes

--- infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/client/prisonersearch/PrisonerSearchClient.kt
+++ infrastructure/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/infrastructure/client/prisonersearch/PrisonerSearchClient.kt
@@ -8,11 +8,15 @@ import org.springframework.web.service.annotation.GetExchange
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.client.ApiCallKeys
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.config.RestClientRetry
 
+// this limits the data that's returned from the prisoner search API to the fields we are interested in
+const val PRISONER_SEARCH_RESPONSE_FIELDS: String =
+  "prisonerNumber,releaseDate,confirmedReleaseDate,inOutStatus,prisonId,prisonName,status"
+
 interface PrisonerSearchClient {
   @GetExchange(value = "/prisoner/{prisonNumber}")
   fun getPrisoner(
     @PathVariable prisonNumber: String,
-    @RequestParam responseFields: String = Prisoner::class.java.declaredFields.joinToString(",") { it.name },
+    @RequestParam responseFields: String = PRISONER_SEARCH_RESPONSE_FIELDS,
   ): Prisoner
 }
 
@@ -21,6 +25,7 @@ interface PrisonerSearchClient {
 class PrisonerSearchCachingService(
   private val prisonerSearchClient: PrisonerSearchClient,
 ) {
+
   @Cacheable(ApiCallKeys.GET_PRISONER, key = "#prisonNumber", sync = true)
   fun getPrisoner(prisonNumber: String) = prisonerSearchClient.getPrisoner(prisonNumber)
 }
