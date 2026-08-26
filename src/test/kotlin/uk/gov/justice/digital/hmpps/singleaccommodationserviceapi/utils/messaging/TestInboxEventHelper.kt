ee654f08bec449acb0fc32d5a11e17367c15709a
daveawc <168838504+daveawc@users.noreply.github.com>
2026-08-26 16:46:49 +0100

    updated publish functions to accept a domain event

--- src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/utils/messaging/TestInboxEventHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/utils/messaging/TestInboxEventHelper.kt
@@ -2,11 +2,16 @@ package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.utils.messagi
 
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import software.amazon.awssdk.services.sns.model.PublishRequest
 import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.readValue
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.messaging.event.IncomingHmppsDomainEventType
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.messaging.event.PersonIdentifier
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.messaging.event.PersonReference
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.messaging.event.SnsDomainEvent
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.entity.ProcessedStatus
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.repository.InboxEventRepository
@@ -14,13 +19,26 @@ import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.justice.hmpps.sqs.MissingTopicException
 import java.time.Duration.ofMillis
 import java.time.Duration.ofSeconds
+import java.time.OffsetDateTime
 
 @Component
 class TestInboxEventHelper(
   private val inboxEventRepository: InboxEventRepository,
   private val jsonMapper: JsonMapper,
   private val hmppsQueueService: HmppsQueueService,
+  @Value($$"${hmpps.sqs.queues.sas-domain-events-queue.subscribeFilter}")
+  private val subscribeFilter: String,
 ) {
+
+  private val subscribeFilterMap: Map<String, List<String>> =
+    jsonMapper.readValue(subscribeFilter)
+
+  private fun validateEventSubscription(eventType: String) {
+    if (!subscribeFilterMap["eventType"]!!.contains(eventType)) {
+      throw IllegalArgumentException("Event type not in subscribe filter: $eventType")
+    }
+  }
+
   private val domainTopic by lazy {
     hmppsQueueService.findByTopicId("hmpps-domain-event-topic")
       ?: throw MissingTopicException("hmpps-domain-event-topic topic not found")
@@ -43,6 +61,7 @@ class TestInboxEventHelper(
   }
 
   fun publish(message: String, eventType: String) {
+    validateEventSubscription(eventType)
     domainTopic.snsClient.publish(
       PublishRequest.builder()
         .topicArn(domainTopic.arn)
@@ -53,6 +72,31 @@ class TestInboxEventHelper(
           ),
         ).build(),
     )
+  }
+
+  fun publish(domainEvent: SnsDomainEvent) {
+    validateEventSubscription(domainEvent.eventType)
+    val message = jsonMapper.writeValueAsString(domainEvent)
+    publish(message, domainEvent.eventType)
+  }
+
+  fun publish(
+    messageType: IncomingHmppsDomainEventType,
+    crn: String?,
+    detailUrl: String?,
+    additionalInformation: Map<String, Any?>? = null,
+  ) {
+    val personReference = crn?.let { PersonReference(listOf(PersonIdentifier("CRN", it))) } ?: PersonReference()
+    val snsDomainEvent = SnsDomainEvent(
+      eventType = messageType.typeName,
+      version = 1,
+      description = "Test ${messageType.typeName} event for $crn",
+      detailUrl = detailUrl,
+      occurredAt = OffsetDateTime.now(),
+      personReference = personReference,
+      additionalInformation = additionalInformation,
+    )
+    publish(snsDomainEvent)
   }
 
   fun assertInboxEvent(
