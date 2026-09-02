96793035a30189aa5a1584113722cc699df6f5a3
daveawc <168838504+daveawc@users.noreply.github.com>
2026-09-02 14:33:07 +0100

    Add Accommodation CAS3 event handler.
    This PR adds in a case refresh trigger for the required CAS3 event types.

---
+++ mutation/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/domain/processor/handler/AccommodationCas3EventHandler.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.domain.processor.handler
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.messaging.event.IncomingHmppsDomainEventType
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.repository.CaseRepository
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CaseRefreshRequestService
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.domain.processor.InboxEventHandler
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.domain.processor.InboxEventHelper
+
+@Component
+class AccommodationCas3EventHandler(
+  private val caseRepository: CaseRepository,
+  private val caseRefreshRequestService: CaseRefreshRequestService?,
+  private val inboxEventHelper: InboxEventHelper,
+) : InboxEventHandler {
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  override fun supportedEventTypes() = setOf(
+    IncomingHmppsDomainEventType.ACCOMMODATION_CAS3_BOOKING_CANCELLED.typeName,
+    IncomingHmppsDomainEventType.ACCOMMODATION_CAS3_BOOKING_CANCELLED_UPDATED.typeName,
+    IncomingHmppsDomainEventType.ACCOMMODATION_CAS3_BOOKING_CONFIRMED.typeName,
+  )
+
+  override fun getPartitionKey(inboxEvent: InboxEventHandler.InboxEvent): String = inboxEventHelper.findCrn(inboxEvent)
+
+  override fun handle(inboxEvent: InboxEventHandler.InboxEvent): InboxEventHandler.Result {
+    val crn = inboxEventHelper.findCrn(inboxEvent)
+    return when (val case = caseRepository.findByCrn(crn)) {
+      null -> InboxEventHandler.Result.IGNORED
+
+      else -> {
+        caseRefreshRequestService?.requestLiveRefresh(case.id)
+        log.info(
+          "APPROVED_PREMISES event processed successfully [inboxEventId={}, crn={}]",
+          inboxEvent.id,
+          crn,
+        )
+        InboxEventHandler.Result.PROCESSED
+      }
+    }
+  }
+}
