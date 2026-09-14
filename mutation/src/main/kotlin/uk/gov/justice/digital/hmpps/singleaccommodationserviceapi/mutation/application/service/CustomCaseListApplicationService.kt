2640468f9d601c55e09fcf41608b62d2b783262c
David McDowall <David.McDowall@justice.gov.uk>
2026-09-14 13:11:08 +0100

    feature/SAS-979-create-custom-case-list-endpoint

---
+++ mutation/src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/application/service/CustomCaseListApplicationService.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.entity.UserCustomCaseListEntity
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.repository.CaseRepository
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.persistence.repository.UserCustomCaseListRepository
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.security.UserService
+
+@Service
+class CustomCaseListApplicationService(
+  private val userService: UserService,
+  private val caseApplicationService: CaseApplicationService,
+  private val caseRepository: CaseRepository,
+  private val userCustomCaseListRepository: UserCustomCaseListRepository,
+  private val caseRefreshRequestService: CaseRefreshRequestService?,
+) {
+  @Transactional
+  fun createCustomCaseList(crns: List<String>) {
+    val user = userService.authorizeAndRetrieveUser()
+    val distinctCrns = crns.distinct()
+
+    caseApplicationService.createCases(distinctCrns.map { CrnToPrisonNumber(it, null) })
+
+    val caseIds = caseRepository.findByCrns(distinctCrns).map { it.id }.distinct()
+
+    userCustomCaseListRepository.deleteBySasUserId(user.id)
+    userCustomCaseListRepository.saveAll(
+      caseIds.map { caseId -> UserCustomCaseListEntity(sasUserId = user.id, sasCaseId = caseId) },
+    )
+
+    caseRefreshRequestService?.requestBulkRefresh(caseIds)
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/api/controller/CustomCaseListController.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.api.controller
+
+import jakarta.validation.ValidationException
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.application.service.CustomCaseListApplicationService
+
+private val CRN_REGEX = Regex("(?i)^[A-Z][0-9]{6}$")
+private const val CRN_FORMAT_MESSAGE = "CRN must be in format A123456"
+
+@Validated
+@RestController
+class CustomCaseListController(
+  private val customCaseListApplicationService: CustomCaseListApplicationService,
+) {
+
+  @PreAuthorize("hasAnyRole('SINGLE_ACCOMMODATION_SERVICE_PROBATION_PRACTITIONER')")
+  @PostMapping("/case-list/custom")
+  @ResponseStatus(HttpStatus.CREATED)
+  fun createCustomCaseList(
+    @RequestBody
+    @NotEmpty(message = "At least one CRN must be provided")
+    @Size(max = 500, message = "A maximum of 500 CRNs can be provided")
+    crns: List<String>,
+  ): ResponseEntity<Void> {
+    val normalisedCrns = crns.map { it.trim().uppercase() }
+    val invalidCrns = normalisedCrns.filterNot { CRN_REGEX.matches(it) }
+
+    if (invalidCrns.isNotEmpty()) {
+      throw ValidationException("$CRN_FORMAT_MESSAGE: ${invalidCrns.joinToString()}")
+    }
+
+    customCaseListApplicationService.createCustomCaseList(normalisedCrns)
+    return ResponseEntity(HttpStatus.CREATED)
+  }
+}
