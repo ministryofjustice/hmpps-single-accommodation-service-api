107687c7ffe5f30c5e2a36f262444fc9d9d4dc36
besscerule <bethan.dobb@justice.gov.uk>
2026-09-14 17:50:57 +0100

    feature/SAS-999-return-correct-service-status-for-cas2-application-for-eligibility-endpoint (#591)

--- query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/unit/eligibility/domain/cas2/suitability/Cas2ApplicationSubmittedRuleTest.kt
+++ query/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/query/unit/eligibility/domain/cas2/suitability/Cas2ApplicationSubmittedRuleTest.kt
@@ -3,21 +3,25 @@ package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.unit.el
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.factories.buildCas2Application
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.infrastructure.factories.buildCas2SubmittedApplicationSummary
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.domain.RuleResult
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.domain.RuleStatus
-import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.domain.cas2.suitability.Cas2ApplicationPresentRule
+import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.eligibility.domain.cas2.suitability.Cas2ApplicationSubmittedRule
 import uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.query.factories.buildDomainData
+import java.time.OffsetDateTime
 
-class Cas2ApplicationPresentRuleTest {
-  private val description = "FAIL if candidate does not have an application"
+class Cas2ApplicationSubmittedRuleTest {
+  private val description = "FAIL if candidate does not have a submitted application"
 
   @Test
-  fun `application is present so rule passes`() {
+  fun `application is submitted so rule passes`() {
     val data = buildDomainData(
-      cas2Application = buildCas2Application(),
+      cas2Application = buildCas2Application(
+        submittedApplication = buildCas2SubmittedApplicationSummary(submittedAt = OffsetDateTime.now()),
+      ),
     )
 
-    val result = Cas2ApplicationPresentRule().evaluate(data)
+    val result = Cas2ApplicationSubmittedRule().evaluate(data)
 
     assertThat(result).isEqualTo(
       RuleResult(
@@ -28,12 +32,14 @@ class Cas2ApplicationPresentRuleTest {
   }
 
   @Test
-  fun `application is not present so rule fails`() {
+  fun `application is not submitted so rule fails`() {
     val data = buildDomainData(
-      cas2Application = null,
+      cas2Application = buildCas2Application(
+        submittedApplication = null,
+      ),
     )
 
-    val result = Cas2ApplicationPresentRule().evaluate(data)
+    val result = Cas2ApplicationSubmittedRule().evaluate(data)
 
     assertThat(result).isEqualTo(
       RuleResult(
