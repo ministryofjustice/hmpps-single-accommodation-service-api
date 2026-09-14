23c885c94b3618ccec45232c8b84d9cff1dd02f8
simonwilson4moj <simon.wilson4@justice.gov.uk>
2026-09-14 15:54:59 +0100

    updated tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/case/CaseControllerIT.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/integration/case/CaseControllerIT.kt
@@ -462,7 +462,7 @@ class CaseControllerIT : IntegrationTestBase() {
       assertThat(nextAccommodation.address.dependentLocality).isEqualTo("AP Area")
       assertThat(nextAccommodation.address.postTown).isEqualTo("AP Town")
 
-      assertThat(createdCase.accommodationStatus).isEqualTo(CaseAccommodationStatus.TRANSIENT)
+      assertThat(createdCase.accommodationStatus).isEqualTo(CaseAccommodationStatus.RISK_OF_NO_FIXED_ABODE)
     }
   }
 
