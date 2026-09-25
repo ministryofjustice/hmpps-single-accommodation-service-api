1f63009ad70a0925fa2fac6509e5d8b0eaa6fac4
Daniel Humphreys <daniel.humphreys@justice.gov.uk>
2026-09-25 15:06:16 +0100

    Group and refactor tests into a new nested class for `arrivePersonAtProposedAccommodation`.

--- mutation/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/unit/domain/aggregate/ProposedAccommodationAggregateTest.kt
+++ mutation/src/test/kotlin/uk/gov/justice/digital/hmpps/singleaccommodationserviceapi/mutation/unit/domain/aggregate/ProposedAccommodationAggregateTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.singleaccommodationserviceapi.mutation.unit.domain.aggregate
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -603,118 +604,123 @@ class ProposedAccommodationAggregateTest {
     assertThat(domainEventsToPublish).hasSize(0)
   }
 
-  @ParameterizedTest
-  @EnumSource(value = AddressStatusCode::class, names = ["PR", "PR1"])
-  fun `should arrive person at proposed accommodation when registered with CPR and proposed address status is PR`(addressStatusCode: AddressStatusCode) {
-    val cprAddressId = UUID.randomUUID()
-    val aggregate = hydrateAggregate(
-      typeVerified = false,
-      cprAddressId = cprAddressId,
-      nextAccommodationStatus = NextAccommodationStatus.YES,
-      accommodationStatus = AccommodationStatusDto(
-        code = addressStatusCode.name,
-        description = addressStatusCode.description,
-      ),
-      startDate = startDate,
-      endDate = endDate,
-    )
-    val arrivalDate = LocalDate.of(2026, 2, 5)
+  @Nested
+  inner class ArrivePersonAtProposedAccommodation {
+    @ParameterizedTest
+    @EnumSource(value = AddressStatusCode::class, names = ["PR", "PR1"])
+    fun `should arrive person at proposed accommodation when registered with CPR and proposed address status is PR`(
+      addressStatusCode: AddressStatusCode,
+    ) {
+      val cprAddressId = UUID.randomUUID()
+      val aggregate = hydrateAggregate(
+        typeVerified = false,
+        cprAddressId = cprAddressId,
+        nextAccommodationStatus = NextAccommodationStatus.YES,
+        accommodationStatus = AccommodationStatusDto(
+          code = addressStatusCode.name,
+          description = addressStatusCode.description,
+        ),
+        startDate = startDate,
+        endDate = endDate,
+      )
+      val arrivalDate = LocalDate.of(2026, 2, 5)
 
-    aggregate.arrivePersonAtProposedAccommodation(arrivalDate, ArrivalMethod.STANDARD)
+      aggregate.arrivePersonAtProposedAccommodation(arrivalDate, ArrivalMethod.STANDARD)
 
-    val aggregateSnapshot = aggregate.snapshot()
-    assertThat(aggregateSnapshot.accommodationStatus?.code).isEqualTo(AddressStatusCode.M.name)
-    assertThat(aggregateSnapshot.accommodationStatus?.description).isEqualTo(AddressStatusCode.M.description)
-    assertThat(aggregateSnapshot.typeVerified).isTrue
-    assertThat(aggregateSnapshot.startDate).isEqualTo(arrivalDate)
-    assertThat(aggregateSnapshot.endDate).isNull()
-    assertThat(aggregateSnapshot.cprAddressId).isEqualTo(cprAddressId)
+      val aggregateSnapshot = aggregate.snapshot()
+      assertThat(aggregateSnapshot.accommodationStatus?.code).isEqualTo(AddressStatusCode.M.name)
+      assertThat(aggregateSnapshot.accommodationStatus?.description).isEqualTo(AddressStatusCode.M.description)
+      assertThat(aggregateSnapshot.typeVerified).isTrue
+      assertThat(aggregateSnapshot.startDate).isEqualTo(arrivalDate)
+      assertThat(aggregateSnapshot.endDate).isNull()
+      assertThat(aggregateSnapshot.cprAddressId).isEqualTo(cprAddressId)
 
-    val domainEventsToPublish = aggregate.pullDomainEvents()
-    assertThat(domainEventsToPublish).hasSize(1)
-    assertThat(domainEventsToPublish.first()).isInstanceOf(AccommodationPersonArrivedDomainEvent::class.java)
-    assertThat(domainEventsToPublish.first().aggregateId).isEqualTo(aggregateSnapshot.id)
-  }
-
-  @Test
-  fun `should throw AccommodationPersonCannotArriveException when proposed accommodation is not registered with CPR`() {
-    val aggregate = hydrateAggregate(
-      cprAddressId = null,
-      typeVerified = false,
-      nextAccommodationStatus = NextAccommodationStatus.YES,
-      accommodationStatus = AccommodationStatusDto(
-        code = AddressStatusCode.PR.name,
-        description = AddressStatusCode.PR.description,
-      ),
-      startDate = startDate,
-      endDate = endDate,
-    )
-
-    assertThrows<AccommodationPersonCannotArriveException> {
-      aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      val domainEventsToPublish = aggregate.pullDomainEvents()
+      assertThat(domainEventsToPublish).hasSize(1)
+      assertThat(domainEventsToPublish.first()).isInstanceOf(AccommodationPersonArrivedDomainEvent::class.java)
+      assertThat(domainEventsToPublish.first().aggregateId).isEqualTo(aggregateSnapshot.id)
     }
 
-    assertThat(aggregate.pullDomainEvents()).isEmpty()
-  }
+    @Test
+    fun `should throw AccommodationPersonCannotArriveException when proposed accommodation is not registered with CPR`() {
+      val aggregate = hydrateAggregate(
+        cprAddressId = null,
+        typeVerified = false,
+        nextAccommodationStatus = NextAccommodationStatus.YES,
+        accommodationStatus = AccommodationStatusDto(
+          code = AddressStatusCode.PR.name,
+          description = AddressStatusCode.PR.description,
+        ),
+        startDate = startDate,
+        endDate = endDate,
+      )
 
-  @Test
-  fun `should throw AccommodationPersonCannotArriveException when proposed accommodation is not next accommodation`() {
-    val aggregate = hydrateAggregate(
-      cprAddressId = UUID.randomUUID(),
-      typeVerified = false,
-      accommodationStatus = AccommodationStatusDto(
-        code = AddressStatusCode.PR.name,
-        description = AddressStatusCode.PR.description,
-      ),
-      nextAccommodationStatus = NextAccommodationStatus.NO,
-      startDate = startDate,
-      endDate = endDate,
-    )
+      assertThrows<AccommodationPersonCannotArriveException> {
+        aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      }
 
-    assertThrows<AccommodationPersonCannotArriveException> {
-      aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      assertThat(aggregate.pullDomainEvents()).isEmpty()
     }
 
-    assertThat(aggregate.pullDomainEvents()).isEmpty()
-  }
+    @Test
+    fun `should throw AccommodationPersonCannotArriveException when proposed accommodation is not next accommodation`() {
+      val aggregate = hydrateAggregate(
+        cprAddressId = UUID.randomUUID(),
+        typeVerified = false,
+        accommodationStatus = AccommodationStatusDto(
+          code = AddressStatusCode.PR.name,
+          description = AddressStatusCode.PR.description,
+        ),
+        nextAccommodationStatus = NextAccommodationStatus.NO,
+        startDate = startDate,
+        endDate = endDate,
+      )
 
-  @Test
-  fun `should throw AccommodationPersonCannotArriveException when accommodation status is not proposed`() {
-    val aggregate = hydrateAggregate(
-      cprAddressId = UUID.randomUUID(),
-      typeVerified = false,
-      accommodationStatus = AccommodationStatusDto(
-        code = AddressStatusCode.M.name,
-        description = AddressStatusCode.M.description,
-      ),
-      nextAccommodationStatus = NextAccommodationStatus.YES,
-      startDate = startDate,
-      endDate = endDate,
-    )
+      assertThrows<AccommodationPersonCannotArriveException> {
+        aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      }
 
-    assertThrows<AccommodationPersonCannotArriveException> {
-      aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), arrivalMethod = ArrivalMethod.STANDARD)
+      assertThat(aggregate.pullDomainEvents()).isEmpty()
     }
 
-    assertThat(aggregate.pullDomainEvents()).isEmpty()
-  }
+    @Test
+    fun `should throw AccommodationPersonCannotArriveException when accommodation status is not proposed`() {
+      val aggregate = hydrateAggregate(
+        cprAddressId = UUID.randomUUID(),
+        typeVerified = false,
+        accommodationStatus = AccommodationStatusDto(
+          code = AddressStatusCode.M.name,
+          description = AddressStatusCode.M.description,
+        ),
+        nextAccommodationStatus = NextAccommodationStatus.YES,
+        startDate = startDate,
+        endDate = endDate,
+      )
 
-  @Test
-  fun `should throw AccommodationPersonCannotArriveException when accommodation status is null`() {
-    val aggregate = hydrateAggregate(
-      cprAddressId = UUID.randomUUID(),
-      typeVerified = false,
-      accommodationStatus = null,
-      nextAccommodationStatus = NextAccommodationStatus.YES,
-      startDate = startDate,
-      endDate = endDate,
-    )
+      assertThrows<AccommodationPersonCannotArriveException> {
+        aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), arrivalMethod = ArrivalMethod.STANDARD)
+      }
 
-    assertThrows<AccommodationPersonCannotArriveException> {
-      aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      assertThat(aggregate.pullDomainEvents()).isEmpty()
     }
 
-    assertThat(aggregate.pullDomainEvents()).isEmpty()
+    @Test
+    fun `should throw AccommodationPersonCannotArriveException when accommodation status is null`() {
+      val aggregate = hydrateAggregate(
+        cprAddressId = UUID.randomUUID(),
+        typeVerified = false,
+        accommodationStatus = null,
+        nextAccommodationStatus = NextAccommodationStatus.YES,
+        startDate = startDate,
+        endDate = endDate,
+      )
+
+      assertThrows<AccommodationPersonCannotArriveException> {
+        aggregate.arrivePersonAtProposedAccommodation(LocalDate.now(), ArrivalMethod.STANDARD)
+      }
+
+      assertThat(aggregate.pullDomainEvents()).isEmpty()
+    }
   }
 
   @Test
